6bf4cf31ecea3d8d96c5760187c70902cb9c37dc
Nfk1 <110776782+Nfk1@users.noreply.github.com>
2025-08-27 01:14:39 +0300

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,19 @@
+// service-worker.js
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open('v1').then(cache => cache.addAll([
+      '/',
+      '/start.html',
+      '/start.css',
+      '/script-start.js',
+      '/icons/icon-192.png',
+      '/icons/icon-512.png'
+    ]))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
